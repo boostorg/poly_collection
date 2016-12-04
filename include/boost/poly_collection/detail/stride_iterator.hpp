a2ab3bf15420130c4cdeceedd2d63ee119bce1af
joaquintides <joaquin@tid.es>
2016-12-04 13:03:53 +0100

    fixed shadowing

--- include/boost/poly_collection/detail/stride_iterator.hpp
+++ include/boost/poly_collection/detail/stride_iterator.hpp
@@ -59,7 +59,7 @@ public:
 
   /* interoperability with [Derived]Value* */
 
-  stride_iterator& operator=(Value* p)noexcept{this->p=p;return *this;}
+  stride_iterator& operator=(Value* p_)noexcept{this->p=p_;return *this;}
   operator Value*()const noexcept{return p;}
 
   template<
