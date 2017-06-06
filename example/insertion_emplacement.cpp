fade58cd3949342395321a67841ebd1afefe7dda
joaquintides <joaquin@tid.es>
2017-06-07 00:48:15 +0200

    used std::discrete_distribution rather than std::uniform_real_distribution

--- example/insertion_emplacement.cpp
+++ example/insertion_emplacement.cpp
@@ -1,4 +1,4 @@
-/* Copyright 2016 Joaquin M Lopez Munoz.
+/* Copyright 2016-2017 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
@@ -20,13 +20,14 @@ int main()
 
   // populate c
 
-  std::mt19937                     gen{92748}; // some arbitrary random seed
-  std::uniform_real_distribution<> rnd;
-  for(int i=0;i<8;++i){ // assign each type with 1/3 probability
-    auto r=rnd(gen);
-         if(r<1.0/3)c.insert(warrior{i});
-    else if(r<2.0/3)c.insert(juggernaut{i});
-    else            c.insert(goblin{i});
+  std::mt19937                 gen{92748}; // some arbitrary random seed
+  std::discrete_distribution<> rnd({1,1,1});
+  for(int i=0;i<8;++i){        // assign each type with 1/3 probability
+    switch(rnd(gen)){ 
+      case 0: c.insert(warrior{i});break;
+      case 1: c.insert(juggernaut{i});break;
+      case 2: c.insert(goblin{i});break;
+    }
   }
 
   auto render=[](const boost::base_collection<sprite>& c){
