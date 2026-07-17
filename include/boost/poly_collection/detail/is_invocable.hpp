6e18217bc90d2bcd4c53d9a3e575760d7e903c12
joaquintides <joaquin@tid.es>
2026-07-17 08:48:03 +0200

    updated (C)

--- include/boost/poly_collection/detail/is_invocable.hpp
+++ include/boost/poly_collection/detail/is_invocable.hpp
@@ -1,4 +1,4 @@
-/* Copyright 2016-2017 Joaquin M Lopez Munoz.
+/* Copyright 2016-2026 Joaquin M Lopez Munoz.
  * Distributed under the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
